9593e3c64bd3cab0074fd30e563312d63b508a5f
sheslitforme <damon@evict.cc>
2024-07-02 18:29:32 -0400

    basis

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord, logging, os, dotenv
+from discord.ext import commands
+
+dotenv.load_dotenv(verbose=True)
+
+token = os.environ['token']
+
+class harm(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(command_prefix=",", owner_ids=[214753146512080907],
+                         intents = discord.Intents.all())
+        
+bot = harm()
+
+@bot.event()
+async def on_ready():
+    print(f"{bot.user.name} has successfully connected to Discord API.")
+    await bot.load_extension('jishaku')
+    await bot.load_extension('cogs.owner')
