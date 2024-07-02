afa7e82cd1179838eb4110ded16d497b96d39f1a
sheslitforme <damon@evict.cc>
2024-07-02 19:04:18 -0400

    quick

--- bot/bot.py
+++ bot/bot.py
@@ -1,14 +1,13 @@
 import discord, discord_ios
 from discord.ext import commands
-
+from bot.startup import Startup
 
 class harm(commands.Bot):
     def __init__(self) -> None:
         super().__init__(command_prefix=",", owner_ids=[214753146512080907],
                          intents = discord.Intents.all(), activity=discord.CustomActivity(name="🔗 harm.cc"))
         
-bot = harm()
 
-async def setup_hook(self):
-    await bot.load_extension('jishaku')
-    await bot.load_extension('cogs.owner')
+    async def setup_hook(self):
+        await self.load_extension('jishaku')
+        await Startup.cogs(self)
