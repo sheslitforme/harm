30ca67affadebe087d67339f17316c5491f7d5b1
sheslitforme <damon@evict.cc>
2024-07-02 21:36:54 -0400

    ya

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,23 @@
-import discord, discord_ios
+import discord, discord_ios, os, asyncpg
 from discord.ext import commands
 from bot.startup import Startup
 from bot.ext import HarmContext
 
 class harm(commands.Bot):
-    def __init__(self) -> None:
+    def __init__(self, db: asyncpg.Pool=None):
         super().__init__(command_prefix=",", owner_ids=[214753146512080907],
                          intents = discord.Intents.all(), allowed_mentions=discord.AllowedMentions(roles=False, everyone=False, users=True, replied_user=False), 
                          activity=discord.CustomActivity(name="🔗 harm.cc"))
         
+        
+        self.db = db
         self.color = 0xffffff
+        
+        self.resent_api = os.environ.get["resent_api"]
+        self.proxy = os.environ.get["proxy"]
+        
+        self.commands_url = os.environ.get("commands_url")
+        self.support_server = os.environ.get("support_server")
         
     async def setup_hook(self):
         await self.load_extension('jishaku')
